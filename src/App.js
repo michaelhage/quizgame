528977ac9b8c2164b60b164a36de77b8ea3eefa2
michael hage <mhage222@gmail.com>
2020-11-13 20:45:27 -0500

    first commit, added navbar and sidebar capability

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Navbar/Sidebar/Sidebar'
+
+import HomePage from './pages/Home'
+import About from './pages/About'
+import Game from './pages/Game'
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+
+
 import './App.css';
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Sidebar isOpen={isOpen} toggle={toggle}/>
+        <Navbar toggle={toggle} />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path='/about-us' exact component={About} />
+          <Route path='/play-quiz' exact component={Game} />
+        </Switch>
+      </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import {
+        Home
+        } from './GeneralPageElements';
+
+function HomePage() {
+    return (
+        <div>
+            <Home>
+                <h1>Home</h1>
+
+            </Home>
+        </div>
+    );
+}
+
+export default HomePage
